4e6d22f83964ef05f83c061599c2ad5ba8fa95b4
Momina Ijaz <mominaijaz169@gmail.com>
2023-12-25 01:40:55 +0500

    manfiest file issue resolved

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
         }),
     ],
     build: {
-        outDir: 'public/build', // Specify the output directory
+        outDir: 'public/build', 
+        manifest: true,
     },
 });
