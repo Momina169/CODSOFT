d015dc97e0c5b3f68d74c8962db0eca2485bd865
Momina Ijaz <mominaijaz169@gmail.com>
2023-12-25 18:18:58 +0500

    configure jawsDB MySql

--- config/database.php
+++ config/database.php
@@ -45,23 +45,22 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 't07cxyau6qg7o5nz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'b506ov9sjij3yx2z'),
+            'username' => env('DB_USERNAME', 'wg57ceithupp4d4d'),
+            'password' => env('DB_PASSWORD', 'g875x2e3zd0nv308'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
+        
 
         'pgsql' => [
             'driver' => 'pgsql',
