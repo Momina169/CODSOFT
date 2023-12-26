94aeda64b5aa0478dc16282f3f48fa231c260029
Momina Ijaz <mominaijaz169@gmail.com>
2023-12-27 03:03:27 +0500

    Force Https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,8 +20,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         
-        if($this->app->environment('production')){
+        if (config('app.env') === 'production') {
             URL::forceScheme('https');
-        }
+
+            $this->app['request']->server->set('HTTPS', true);
     }
 }
