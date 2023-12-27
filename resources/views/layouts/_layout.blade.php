7fd6f94db0121b33e57cabb04499b1574c505b16
Momina Ijaz <mominaijaz169@gmail.com>
2023-12-28 02:33:40 +0500

    update

--- resources/views/layouts/_layout.blade.php
+++ resources/views/layouts/_layout.blade.php
@@ -11,8 +11,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Favicons -->
-    <link href="{{ secure_asset('assets/img/favicon.png') }}" rel="icon">
-    <link href="{{ secure_asset('assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+    <link href="{{ secure_asset('images/logo.png') }}" rel="icon">
     <!-- <link rel="preload" as="style" href="{{ secure_asset('build/assets/app-e802707b.css') }}" />
     <link rel="modulepreload" href="{{ secure_asset('build/assets/app-4a08c204.js') }}" /> -->
 </head>
@@ -29,8 +28,6 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
 
-    <script src="{{ secure_asset('cdn.jsdelivr.net_npm.js') }}"></script>
-    <script src="{{ secure_asset('js_bootstrap.min.js') }}"></script>
     <script src="{{ secure_asset('sidebars.js') }}"></script>
 
 </html>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -25,11 +25,105 @@
             </button>
 
             <!-- Navigation items on the right -->
-            <div id="main-nav">
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <!-- Your menu items here -->
-                </ul>
-            </div>
-        </div>
-    </nav>
+          <div class="collapse navbar-collapse" id="main-nav">
+              <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                      <a class="nav-link " aria-current="page" href="{{route('index')}}">Home</a>
+                  </li>
+
+                  <li class="nav-item"><a class="nav-link" href="{{route('finddoctor')}}">Find Doctors</a></li>
+
+                  <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                          aria-expanded="false">
+                          Our Purpose
+                      </a>
+                      <ul class="dropdown-menu">
+                          <li><a class="dropdown-item" href="{{route('mission')}}">Our Mission</a></li>
+                          <li>
+                              <hr class="dropdown-divider">
+                          </li>
+
+                          <li><a class="dropdown-item" href="{{route('vision')}}">Our Vision</a></li>
+
+                      </ul>
+                  </li>
+
+
+                  <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                          aria-expanded="false">
+                          Our Team
+                      </a>
+                      <ul class="dropdown-menu">
+                          <li><a class="dropdown-item" href="#">Doctors</a></li>
+                          <li>
+                              <hr class="dropdown-divider">
+                          </li>
+
+                          <li><a class="dropdown-item" href="{{route('nurses')}}">Nurses</a></li>
+                          <li>
+                              <hr class="dropdown-divider">
+                          </li>
+
+                          <li><a class="dropdown-item" href="{{route('sweepers')}}">Sweepers</a></li>
+                      </ul>
+                  </li>
+                  <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                          aria-expanded="false">
+                          Departments
+                      </a>
+                      <ul class="dropdown-menu">
+                          <li><a class="dropdown-item" href="{{route('surgries')}}">Surgery</a></li>
+                          <li>
+                              <hr class="dropdown-divider">
+                          </li>
+
+                          <li><a class="dropdown-item" href="{{route('opd')}}">OPD</a></li>
+                          <li>
+                              <hr class="dropdown-divider">
+                          </li>
+                          <li><a class="dropdown-item" href="{{route('labs')}}">Labs</a></li>
+
+                      </ul>
+                  </li>
+
+                  <li class="nav-item">
+                      <a class="nav-link " aria-current="page" href="{{route('contact')}}">Contact us</a>
+                  </li>
+
+                  <li class=" nav-item  dropdown">
+                      <a class="nav-link dropdown-toggle py-0 px-2 btn " href="#" role="button"
+                          data-bs-toggle="dropdown" aria-expanded="false">
+                          <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}
+                          </div>
+                          <div class="font-medium text-sm text-gray-500 " hidden>{{ Auth::user()->email }}</div>
+                      </a>
+
+
+                      <ul class="dropdown-menu">
+                          <li><a class="dropdown-item ms-2 " href="{{route('profile.edit')}}">Profile'</a></li>
+
+                          <!-- Authentication -->
+                          <form method="POST" action="{{ route('logout') }}">
+                              @csrf
+                              <li><a class="dropdown-item" href="{{route('logout')}}">
+                                      <x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault();
+                                        this.closest('form').submit();">
+                                          {{ __( ' Log Out') }}
+                                      </x-responsive-nav-link>
+                                  </a></li>
+                          </form>
+                      </ul>
+
+                  </li>
+
+
+              </ul>
+
+          </div>
+          </div>
+      </nav>
+
 
