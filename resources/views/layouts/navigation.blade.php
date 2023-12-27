7612ec45c335de6ffb0151fc08977edfa86efb10
Momina Ijaz <mominaijaz169@gmail.com>
2023-12-28 03:58:06 +0500

    update

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,52 +1,42 @@
-<nav x-data="{ open: false }" class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
         <a class="navbar-brand" href="{{ route('dashboard') }}">
             {{ __('Medical Haven') }}
         </a>
-        <button @click="open = ! open" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
 
-        <div :class="{'collapse': ! open, 'navbar-collapse': true}" id="navbarNav">
-            <!-- Navigation Links -->
-            <ul class="navbar-nav">
-                <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                    {{ __('Dashboard') }}
-                </x-nav-link>
-                <x-nav-link :href="route('index')" :active="request()->routeIs('dashboard')">
-                    {{ __('Home') }}
-                </x-nav-link>
-            </ul>
+        <!-- Navigation Links -->
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link " aria-current="page" href="{{route('dashboard')}}">DashBoard</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link " aria-current="page" href="{{route('index')}}">Home</a>
+            </li>
+        </ul>
 
-            <!-- Settings Dropdown -->
-            <div class="ms-auto">
-                <x-dropdown align="right" width="48">
-                    <x-slot name="trigger">
-                        <button class="btn btn-link" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown"
-                            aria-haspopup="true" aria-expanded="false">
-                            {{ Auth::user()->name }}
-                        </button>
-                    </x-slot>
+        <!-- Settings Dropdown -->
+        <div class="ms-auto float-right">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                    aria-expanded="false">
+                    <button> {{ Auth::user()->name }} </button>
+                </a>
 
-                    <x-slot name="content">
-                        <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Profile') }}
-                        </x-dropdown-link>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="{{ route(profile.edit) }}">Profile</a></li>
 
-                        <!-- Authentication -->
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
+                    <!-- Authentication -->
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <li>
+                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                                 {{ __('Log Out') }}
-                            </x-dropdown-link>
-                        </form>
-                    </x-slot>
-                </x-dropdown>
-            </div>
+                            </a>
+                        </li>
+                    </form>
+                </ul>
+            </li>
         </div>
     </div>
 </nav>
