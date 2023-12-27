c61e3b71ca4fa5fe50bf95d79183993739edc9e3
Momina Ijaz <mominaijaz169@gmail.com>
2023-12-28 03:14:51 +0500

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,6 @@
     <title>{{ config('app.name', 'Management System') }}</title>
 
     <script src="https://kit.fontawesome.com/275c3c76a3.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="{{ secure_asset('build/assets/app-e802707b.css') }}" />
     <link rel="stylesheet" href="{{ secure_asset('css_bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ secure_asset('style.css') }}">
     <link rel="stylesheet" href="{{ secure_asset('/sidebars.css') }}">
@@ -22,9 +21,10 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-    <link rel="preload" as="style" href="{{ secure_asset('build/assets/app-e802707b.css') }}" as="style" />
+    <!-- <link rel="preload" as="style" href="{{ secure_asset('build/assets/app-e802707b.css') }}" as="style" />
     <link rel="modulepreload" href="{{ secure_asset('build/assets/app-4a08c204.js') }}" />
-    <script type="module" src="{{ secure_asset('build/assets/app-4a08c204.js') }}"></script>
+    <link rel="stylesheet" href="{{ secure_asset('build/assets/app-e802707b.css') }}" />
+    <script type="module" src="{{ secure_asset('build/assets/app-4a08c204.js') }}"></script> -->
     <style>
     .admin-sidebar {
         max-width: 20%;
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,3 +1,5 @@
+<x-app-layout>
+
 <nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 my-3">
@@ -95,3 +97,4 @@
         </div>
     </div>
 </nav>
+</x-app-layout>
