62e080c4d98e716ec9674243f8483ec79dfb1263
Momina Ijaz <mominaijaz169@gmail.com>
2023-12-29 03:01:26 +0500

    update

--- resources/views/layouts/_layout.blade.php
+++ resources/views/layouts/_layout.blade.php
@@ -6,6 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/275c3c76a3.js" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <link rel="stylesheet" href="{{ secure_asset('css_bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ secure_asset('style.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
