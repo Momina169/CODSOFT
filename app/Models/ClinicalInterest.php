4e4cf736924f586ce95619965b7471f970c2482a
Momina Ijaz <mominaijaz169@gmail.com>
2023-12-28 05:05:16 +0500

    model issue

--- app/Models/ClinicalInterest.php
+++ app/Models/ClinicalInterest.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class clinicalInterest extends Model
+class ClinicalInterest extends Model
 {
     use HasFactory;
 
